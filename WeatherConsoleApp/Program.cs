df8c1e90bdc48252cd630d6002b2b33415c1a655
Nestor <nestorkhanas@gmail.com>
2025-07-02 16:06:18 +0300

    Create WeatherService

--- WeatherConsoleApp/Program.cs
+++ WeatherConsoleApp/Program.cs
@@ -1,6 +1,5 @@
-﻿using System.ComponentModel;
-using System.Text.Json;
-using WeatherConsoleApp.Models;
+﻿using WeatherConsoleApp.Models;
+using WeatherConsoleApp.Services;
 
 string apiKey = "3a605f068da62859e080c491f677fee5";
 string city = "London";
@@ -11,7 +10,7 @@ while (true)
     Console.Write("Input Your city:");
     city = Console.ReadLine() ?? city;
     
-    WeatherRoot showingWeatherData = GetWeather(apiKey, city);
+    WeatherRoot showingWeatherData = WeatherService.GetWeather(apiKey, city);
 
 
     if (showingWeatherData != null)
@@ -26,25 +25,5 @@ while (true)
     {
         Console.WriteLine("Failed to retrieve weather data.");
 
-    }
-}
-
-
-WeatherRoot? GetWeather(string apiKey, string city)
-{
-    using (var client = new HttpClient())
-    {
-        client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
-        var response = client.GetAsync($"weather?q={city}&appid={apiKey}&units=metric").Result;
-        if (response.IsSuccessStatusCode)
-        {
-            var json = response.Content.ReadAsStringAsync().Result;
-            var weatherData = JsonSerializer.Deserialize<WeatherRoot>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            return weatherData;
-        }
-        else
-        {
-            throw new Exception($"Error fetching weather data: {response.ReasonPhrase}");
-        }
     }
 }
---
+++ WeatherConsoleApp/Services/WeatherService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using WeatherConsoleApp.Models;
+
+namespace WeatherConsoleApp.Services
+{
+    class WeatherService
+    {
+        static public WeatherRoot? GetWeather(string apiKey, string city)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+                var response = client.GetAsync($"weather?q={city}&appid={apiKey}&units=metric").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = response.Content.ReadAsStringAsync().Result;
+                    var weatherData = JsonSerializer.Deserialize<WeatherRoot>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    return weatherData;
+                }
+                else
+                {
+                    throw new Exception($"Error fetching weather data: {response.ReasonPhrase}");
+                }
+            }
+        }
+    }
+}
