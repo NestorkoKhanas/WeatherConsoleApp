51fe2d4ee5879e7ea051635d8fb6cc4d163426af
Nestor <nestorkhanas@gmail.com>
2025-07-02 16:01:17 +0300

    Create Weather Model

---
+++ WeatherConsoleApp/Models/WeatherRoot.cs
@@ -0,0 +1,36 @@
+﻿namespace WeatherConsoleApp.Models
+{
+    class WeatherRoot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public MainWeather Main { get; set; }
+        public List<Weather> Weather { get; set; }
+        public Wind Wind { get; set; }
+        public Coordinates Coord { get; set; }
+    }
+    class MainWeather
+    {
+        public double Temp { get; set; }
+        public double Humidity { get; set; }
+        public double SeaLevel { get; set; }
+        public double TempMax { get; set; }
+        public double TempMin { get; set; }
+
+    }
+    class Weather
+    {
+        public string Description { get; set; }
+    }
+    class Wind
+    {
+        public double Speed { get; set; }
+        public double Deg { get; set; }
+        public double Gust { get; set; }
+    }
+    class Coordinates
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+}
--- WeatherConsoleApp/Program.cs
+++ WeatherConsoleApp/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using System.ComponentModel;
+using System.Text.Json;
+using WeatherConsoleApp.Models;
+
+string apiKey = "3a605f068da62859e080c491f677fee5";
+string city = "London";
+
+while (true)
+{
+
+    Console.Write("Input Your city:");
+    city = Console.ReadLine() ?? city;
+    
+    WeatherRoot showingWeatherData = GetWeather(apiKey, city);
+
+
+    if (showingWeatherData != null)
+    {
+        Console.WriteLine($"City: {showingWeatherData.Name}");
+        Console.WriteLine($"Temperature: {showingWeatherData.Main.Temp}°C");
+        Console.WriteLine($"Humidity: {showingWeatherData.Main.Humidity}%");
+        Console.WriteLine($"Weather: {showingWeatherData.Weather[0].Description}");
+        Console.WriteLine($"Wind Speed: {showingWeatherData.Wind.Speed} m/s");
+    }
+    else
+    {
+        Console.WriteLine("Failed to retrieve weather data.");
+
+    }
+}
+
+
+WeatherRoot? GetWeather(string apiKey, string city)
+{
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+        var response = client.GetAsync($"weather?q={city}&appid={apiKey}&units=metric").Result;
+        if (response.IsSuccessStatusCode)
+        {
+            var json = response.Content.ReadAsStringAsync().Result;
+            var weatherData = JsonSerializer.Deserialize<WeatherRoot>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            return weatherData;
+        }
+        else
+        {
+            throw new Exception($"Error fetching weather data: {response.ReasonPhrase}");
+        }
+    }
+}
